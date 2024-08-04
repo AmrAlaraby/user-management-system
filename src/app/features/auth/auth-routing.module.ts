0f8bef01d14a9488830ab373fff49ed4b82ccbdd
AmrAlaraby <amr2amr3@gmail.com>
2024-08-04 09:47:55 -0700

    routs

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -4,6 +4,9 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
+    path:'',component:LoginComponent
+  },
+  {
     path:'login',component:LoginComponent
   }
 ];
