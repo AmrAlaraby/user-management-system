99eb58ba5b6f6b83833bc7da4b0442e905457733
AmrAlaraby <amr2amr3@gmail.com>
2024-08-15 10:53:05 -0700

    final

--- src/app/features/users/components/update-user/update-user.component.ts
+++ src/app/features/users/components/update-user/update-user.component.ts
@@ -1,4 +1,5 @@
-import { User } from './../../interfaces/user';
+import { User } from 'src/app/features/users/interfaces/user';
+
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -13,14 +14,18 @@ export class UpdateUserComponent {
   errorMessage=''
   loginForm!:FormGroup
   id:number=0
+  myUser!:User
 constructor(private _activatedRoute:ActivatedRoute,private _formBuilder:FormBuilder,private _userService:UsersService,private _router:Router){}
 
   ngOnInit(): void {
+
     this._activatedRoute.params.subscribe((param:any)=>{
       console.log(param);
       this.id=param.user
+      
       })
     this.initLoginForm()
+    this.getMyUser()
   }
   initLoginForm(){
     this.loginForm= this._formBuilder.group({
@@ -30,6 +35,25 @@ constructor(private _activatedRoute:ActivatedRoute,private _formBuilder:FormBuil
       email:['',Validators.required], 
       phone:['',Validators.required],
       birthDate:['',Validators.required]
+    })
+  }
+  getMyUser()
+  {
+    this._userService.getUserById(this.id).subscribe({
+      next : res=>{
+
+         console.log(res)
+         this.myUser=res
+         console.log(this.myUser);
+         this.loginForm.patchValue({...res})
+         
+
+      },
+      error:err=> {
+    this.errorMessage=err.error.message;
+    
+      }
+      
     })
   }
   submitLogin(){
